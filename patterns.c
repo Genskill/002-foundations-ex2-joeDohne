8ef275528b4409f93ed417cb01c6b78f1f4dbc02
joeDohne <j03.d0hne@gmail.com>
2021-05-18 11:16:25 +0000

    msg

--- patterns.c
+++ patterns.c
@@ -3,7 +3,7 @@
 
 int main(void){
     int type = get_int("Enter type of pattern (1 or 2) ");
-    int rows = get_int("Number of rows ");
+    int rows = get_int("Number of rows");
     
 
     for (int i = rows; i > 0; i--){
